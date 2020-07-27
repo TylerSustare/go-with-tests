9d8c41d752748a4746c8db326d3e6f3a8bb74590
TylerSustare <tylersustare@gmail.com>
2020-07-26 20:57:08 -0700

    WIP maps

--- structs_methods_and_interfaces/shapes_test.go
+++ structs_methods_and_interfaces/shapes_test.go
@@ -21,19 +21,15 @@ func TestArea(t *testing.T) {
 	}{
 		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
 		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea 36.0},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 	for _, tt := range areaTests {
 		// using tt.name from the case to use it as the `t.Run` test name
-		t.Run(tt.name, func(t *testing.T){
+		t.Run(tt.name, func(t *testing.T) {
 			got := tt.shape.Area()
-			if got != tt.hasArea{
+			if got != tt.hasArea {
 				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 			}
 		})
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
 	}
 }
